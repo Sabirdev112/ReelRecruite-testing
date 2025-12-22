431e7af00076cdd2d3d4c7daf44db03a3e4ac7aa
Khurram262 <144933675+Khurram262@users.noreply.github.com>
2025-12-22 19:41:59 +0500

    Login with phone files created

--- Pages/Login.js
+++ Pages/Login.js
@@ -1,7 +1,10 @@
 export class Login {
     constructor(page) {
         this.page = page;
+        
         this.usernameInput = page.getByRole('textbox', { name: 'Enter your email' });
+        this.phonePage = page.getByLabel('Phone');
+        this.phoneInput = page.getByRole('textbox', { name: 'Enter phone number' });
         this.passwordInput = page.getByRole('textbox', { name: 'Enter your password' });
         this.signInButtonLocator = page.locator('xpath=//*[@id="root"]/div[2]/div[2]/div/div[2]/form/button');
         this.avatar = page.locator("//p[@class='text-xs capitalize text-gray-500']");
@@ -10,7 +13,14 @@ export class Login {
     async goto() {
         await this.page.goto('https://recruitai-web-production.up.railway.app/auth');
     }
+
+    async phoneLogin(phone) {
+        await this.phonePage.click();
+        await this.phoneInput.pressSequentially(phone, { delay: 40 });
+        await this.phoneInput.blur();
+    }
     async login(email, password) {
+        
         await this.usernameInput.waitFor({ state: 'visible' });
         await this.usernameInput.click();
         await this.usernameInput.pressSequentially(email, { delay: 40 });
